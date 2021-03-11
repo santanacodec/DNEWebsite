e09e1674b8adfbea34cc387549f9148313601af4
Jopanzo <jpnzo2k12@gmail.com>
2021-03-11 00:39:43 -0500

    Added Model Serializers for API calls

--- dnewebsite/blog/views.py
+++ dnewebsite/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import blog
+from .serializers import blogSerializer
+from rest_framework import generics
+
+class blogListCreate(generics.ListCreateAPIView):
+    queryset = blog.objects.all()
+    serializer_class = blogSerializer
+
 
 # Create your views here.
