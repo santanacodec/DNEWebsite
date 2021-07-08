8649cd79d2b4fe826fb16af3271685c91f12bb86
Jopanzo <jpnzo2k12@gmail.com>
2021-07-08 14:39:10 -0400

    ok

--- dnewebsite/blog/admin.py
+++ dnewebsite/blog/admin.py
@@ -4,5 +4,5 @@ from .models import blog
 # Register your models here.
 @admin.register(blog)
 class blogAdminModel(admin.ModelAdmin):
-    list_filter=('blogAuthor','blogContent')
-    list_display =('blogAuthor','blogContent')
+    list_filter=('blogAuthor','blogContent') #This lets you filter by field
+    list_display =('blogAuthor','blogContent')#This is to diplay field names in admin page
