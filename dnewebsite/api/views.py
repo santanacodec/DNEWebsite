c08b051f1938b8d137ee026da4335d9933f7655c
Jopanzo <jpnzo2k12@gmail.com>
2021-07-08 23:24:45 -0400

    ok

--- dnewebsite/api/views.py
+++ dnewebsite/api/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import http.client
 
+conn = http.client.HTTPSConnection("instagram47.p.rapidapi.com")
+
+headers = { 'x-rapidapi-host': "instagram47.p.rapidapi.com" }
+
+conn.request("GET", "/post_comments?postid=2435143128484144113", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
 # Create your views here.
 def Index(request):
-    return HttpResponse("It is working")
+    return HttpResponse(data.decode("utf-8"))
