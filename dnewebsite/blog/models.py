53ab1d01aed4b7f1c4eddc73a73b0ba39517edb2
Jopanzo <jpnzo2k12@gmail.com>
2021-07-13 14:21:40 -0400

    Added field to Blog(DTF) made model for Spotify

--- dnewebsite/blog/models.py
+++ dnewebsite/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class blog(models.Model):
     blogAuthor = models.CharField(max_length=30)
     blogContent = models.CharField(max_length=100)
+    blogDTF = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
--- dnewebsite/spotify/models.py
+++ dnewebsite/spotify/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50,unique=True)
+    created_at = models.DateField(auto_now_add=True)
+    refresh_token = models.CharField(max_length=150)
