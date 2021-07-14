2612a8b8b6504bf27bd6267ee29b35c757bea8e0
Jopanzo <jpnzo2k12@gmail.com>
2021-07-14 12:34:43 -0400

    ok

--- dnewebsite/spotify/models.py
+++ dnewebsite/spotify/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50,unique=True)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     refresh_token = models.CharField(max_length=150)
+    access_token = models.CharField(max_length=150)
+    expires_in = models.DateTimeField()
+    token_type = models.CharField(max_length=50)
