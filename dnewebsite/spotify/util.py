4167d17b6a40429ca70692b46643d399f28fcebd
Jopanzo <jpnzo2k12@gmail.com>
2021-07-14 12:32:14 -0400

    OK

--- dnewebsite/spotify/util.py
+++ dnewebsite/spotify/util.py
@@ -1,1 +1,15 @@
 from .models import SpotifyToken
+from django.utils import timezone
+from datetime import timedelta
+
+def get_user_token(session_id):
+    user_token = SpotifyToken.objects.filter(user = session_id)
+    if user_token.exists():
+        return user_token[0]
+    else:
+        return None
+def something(session_id,access_token, token_type,expires_in):
+    token = get_user_token(session_id)
+    expires_in = timezone.now + timedelta(seconds=7200)
+    if token:
+        
