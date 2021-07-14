2fa7b5d663d88511174c313df3b31ebe961dcee9
Jopanzo <jpnzo2k12@gmail.com>
2021-07-13 22:43:15 -0400

    OK

--- dnewebsite/blog/models.py
+++ dnewebsite/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class blog(models.Model):
     blogAuthor = models.CharField(max_length=30)
     blogContent = models.CharField(max_length=100)
-    blogDTF = models.DateField(auto_now_add=True)
+    blogDTF = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
